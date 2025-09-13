f68baba90f7b0001086d3dedc3fee575a3ae9705
Dev-Jhuvel <devjhuvel@gmail.com>
2025-09-13 21:45:14 +0800

    install cron

--- src/server.js
+++ src/server.js
@@ -13,7 +13,9 @@ const PORT = process.env.PORT;
 // TODO: PRACTICE ACTIVE RECALL
 // middleware
 app.use(rateLimiter);
-app.use(express.json());
+app.use(express.json({
+    limit: "50mb",
+  }));
 
 app.use("/api/transactions", transactionRoute);
 app.use("/api/health", (req, res) => {
